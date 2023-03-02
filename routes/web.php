60334466557ea0c2cd76b96fbb70a9f75eacd621
Ilya Kuzimn <ilyacool@mail.ru>
2023-03-02 11:47:11 +0700

    Add routes in Project. Create Home, About and Contact in views

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/laravel', function () {
     return view('welcome');
 });
