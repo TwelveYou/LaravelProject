6ff50aea14466167efdf22769f5187f270b416e6
Ilya Kuzimn <ilyacool@mail.ru>
2023-03-06 10:38:32 +0700

    Drop footer in botom page. Implemented by wrappers

--- resources/views/inc/if.blade.php
+++ resources/views/inc/if.blade.php
@@ -1,4 +1,6 @@
-<div class='container'> 
-    <h2> Блок условия </h2>
-    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ullam rerum dicta fuga non distinctio.</p>
+<div class='jumbotron bg-secondary' style="--bs-bg-opacity: .3;">
+    <div class='container'> 
+        <h2> Блок условия </h2>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ullam rerum dicta fuga non distinctio.</p>
+    </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,29 +6,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title-block')</title>
     <link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">   
 </head>
 <body>
-    @include('inc.header')
-    
-    @if(Request::is('/'))
-        @include('inc.if')
-    @endif
+    <div class='no-footer' style='min-height: 100vh;'>        
+        @include('inc.header')
+        
+        @if(Request::is('/'))
+            @include('inc.if')
+        @endif
 
-    <div class='container mt-5 mb-5'>
-        <div class='row'>
-            <div class='col-8'>
-                @yield('content')
-            </div>
-            <div class='col-4'>
-                @include('inc/aside')
+        <div class='container mt-5 mb-5'>
+            <div class='row'>
+                <div class='col-8'>
+                    @yield('content')
+                </div>
+                <div class='col-4'>
+                    @include('inc/aside')
+                </div>
             </div>
         </div>
     </div>
-
-
-
-
-    @include('inc.footer')
+    <div style='margin-top: -145px; max-height: 145px;'>        
+        @include('inc.footer')
+    </div>
 </body>
 </html>
