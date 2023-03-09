b7ef6e0a87b0be65562a2339f17362f946deea77
Ilya Kuzimn <ilyacool@mail.ru>
2023-03-09 14:41:51 +0700

    Realize write in DateBase data from $req(request on view (Contact) with form "resources\views\inc\contact_form.blade.php") by controller "app\Html\Controllers\ContactController.php"

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\ContactRequest;
+use App\Http\Requests\ContactRequest; 
+use App\Models\Contact;
 
 class ContactController extends Controller{
     public function submit(){
@@ -15,12 +16,15 @@ class ContactController extends Controller{
     }
 
     public function valid(ContactRequest $req){
-        // $validation = $req->validate([
-        //     'subject' => 'required|min:5|max:50',
-        //     'message' => 'required|min:15|max:500'
-        // ]);
+        $contact = new Contact(); // создаем объект класса модели Contact
+        $contact->name = $req->input('name');
+        $contact->email = $req->input('email');
+        $contact->subject = $req->input('subject');
+        $contact->message = $req->input('message');
 
-        
+        $contact->save();
+
+        return redirect()->route('home');
     }
 
 }
